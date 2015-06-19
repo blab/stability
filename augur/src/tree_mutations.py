30f732d78dfdb76d0aabb899dea27c644240ac37
Charlton Callender <chacalle@uw.edu>
2015-06-19 10:22:29 -0700

    tree_mutations prints mutations
    Prints background sequence, mutation information and trunk.

--- augur/src/tree_mutations.py
+++ augur/src/tree_mutations.py
@@ -7,12 +7,21 @@ class tree_mutations(object):
 	def __init__(self, **kwargs):
 		'''
 		'''
-
+		
 	def catalog_mutations(self):
 		'''
 		run through and print mutations
-		'''
+
+		'''                     
+		
 		import pprint
 		pp = pprint.PrettyPrinter(indent=4)
-		for node in self.tree.postorder_node_iter():
-			pp.pprint(vars(node))
+		for node in self.tree.postorder_internal_node_iter():
+			sequence = str(node.seq)
+                        for child in node.child_nodes():
+        					mutation = str(child.aa_muts)
+        					trunk = str(child.trunk)
+        					#pp.pprint(mutation + '\n' + trunk + '\n' + sequence)
+        					pp.pprint(mutation)
+        					pp.pprint(trunk)
+        					pp.pprint(sequence)
