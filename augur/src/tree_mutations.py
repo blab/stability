d5259bb223ca8036c3df5cea68b8c695f7b9d854
Charlton Callender <chacalle@uw.edu>
2015-07-09 11:39:23 -0700

    * got tree_mutations working again to restrict to specific site ranges. Next need to get multiple mutations fixed

--- augur/src/tree_mutations.py
+++ augur/src/tree_mutations.py
@@ -17,38 +17,47 @@ class tree_mutations(object):
         '''
         mutation_trunk_fileName = "mutation_trunk.txt"
         mutation_trunk_file = open(mutation_trunk_fileName, 'w')
-		
-		# change depending on protein structure/outgroup you are using
-		lowerRange = 9
-		upperRange = 501
-		
+
+        # change depending on protein structure/outgroup you are using
+        lowerRange = 9
+        upperRange = 501
+
+
+        def check_siterange(mut):
+            site = int(mut[1:len(mut) - 1])
+            #print(mut + "->" + str(site))
+            if lowerRange <= site <= upperRange:
+                return mut + ","
+            else:
+                return ""
+
+        def update_mutations(current_node_mutations, current_total_mutations):
+            # for formatting purposes only want to add the new mutation to the list if it is not blank
+            if str(current_node_mutations) != "":
+                currentMutation = str(current_node_mutations).split(",")
+                for mut in currentMutation:
+                    current_total_mutations += check_siterange(mut)
+            return current_total_mutations
+
         def node_foldx(self, node, mutation):
             if node.parent_node is None:  # root of the tree
                 for child in node.child_nodes():
                     #print("hi")
-                    print(child.aa_seq)
-                    print(child.aa_muts)
-                    '''
-                    print(child.aa_muts) 
-                    this will print out all the mutations needed to get from the 
-                    root/structure to the first child nodes so that foldX should find all 
-                    mutations needed. 
-                    '''
+                    #print(child.aa_seq)
+                    #print(child.aa_muts)
+                    mutation = update_mutations(child.aa_muts, mutation)
+                    # these are the mutations needed to make the structure equal to the first node
+                    mutation_trunk_file.write("RootMut" + "\t" + mutation[:len(mutation) - 1] + ";\n")
+                    print("RootMut" + "\t" + mutation[:len(mutation) - 1] + ";\n")
                     node_foldx(self, child, "")
             else:  # internal or leaf node
                 for child in node.child_nodes():
-                        trunk = str(child.trunk)
-                        # for formatting purposes only want to add the new mutation to the list if it is not blank
-                        if str(child.aa_muts) != "":
-                            currentMutation = str(child.aa_muts).split(",")
-                            for mut in currentMutation:
-                                # need to limit to range of protein structure, 9-501
-                                site = mut[1:len(mut)]
-                                if site >= lowerRange and site <= upperRange:
-                                	mutation += mut + ","
-                        print(mutation)
-                        mutation_trunk_file.write(trunk + "\n" + mutation + ";\n")
-                        node_foldx(self, child, mutation)
+                    trunk = str(child.trunk)
+                    # for formatting purposes only want to add the new mutation to the list if it is not blank
+                    mutation = update_mutations(child.aa_muts, mutation)
+                    print(trunk + "\t" + mutation[:len(mutation) - 1] +";\n")
+                    mutation_trunk_file.write(trunk + "\t" + mutation[:len(mutation) - 1] +";\n")
+                    node_foldx(self, child, mutation)
 
 
         node_foldx(self, self.tree.seed_node, "")
