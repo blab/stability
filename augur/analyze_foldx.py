f3046c5df02d90152b8e42dac1a785296b842862
Charlton Callender <chacalle@uw.edu>
2015-07-22 16:08:53 -0700

    Plot trunk and side branches, fixed bug in printing mutations
    * Fixed bug in tree_mutations in order to print the correct mutations
    * Added to analyze_foldx to automatically plot the main trunk pathway
    stability and its adjacent side branches

--- augur/analyze_foldx.py
+++ augur/analyze_foldx.py
@@ -9,50 +9,28 @@ def make_run_file(name_extension):
     pdb_file_name = pdb_name + name_extension + ".pdb"
     runfileName = "stability_runfile.txt"
     runfile = open(runfileName, 'w')
-    runfile.write('<TITLE>FOLDX_runscript;\n<JOBSTART>#;\n<PDBS>%s;\n<BATCH>#;\n<COMMANDS>FOLDX_commandfile;\n<Stability>%s;\n<END>#;\n<OPTIONS>FOLDX_optionfile;\n<END>#;\n<JOBEND>#;\n<ENDFILE>#;' % (pdb_file_name, "stability.txt"))
+    runfile.write('<TITLE>FOLDX_runscript;\n<JOBSTART>#;\n<PDBS>%s;\n<BATCH>#;\n<COMMANDS>FOLDX_commandfile;\n<Stability>%s;\n<END>#;\n<OPTIONS>FOLDX_optionfile;\n<END>#;\n<JOBEND>#;\n<ENDFILE>#;' % (pdb_file_name, "Stability.txt"))
 
 # opens the output of the stability command in foldX and gets the dG value for the current structure
 def get_stability():
-    stability_file_name = "stability.txt"
+    stability_file_name = "Stability.txt"
     stability_file = open(stability_file_name, 'r')
     for line in stability_file:
-        if line[:5] == pdb_name + "_":
+        if line.startswith(pdb_name):
             stability_line = line.split()
             stability = stability_line[1]
     stability_file.close()
     return float(stability)
 
-def get_trunk_ddG(stability):
-    ddG_mutation = "ddG_mutations.txt"
-    ddG_mutation_file = open(ddG_mutation, 'r')
-    trunk_ddG = "trunk_ddG.txt"
-    trunk_ddG_file = open(trunk_ddG, 'w')
-    trunk_ddG_file.write(str(stability) + "\n")
-    list_stability = [stability]
-    trunk_ddG_list = []
-    nontrunk_ddG_list = []
-    for line in ddG_mutation_file:
-        trunk_line = line.split()
-        if trunk_line[0] == "True":
-            trunk_ddG = float(trunk_line[1])
-            trunk_ddG_list.append(trunk_ddG)
-            mutations = trunk_line[2]
-            dG = trunk_ddG + stability
-            trunk_ddG_file.write(str(dG) + "\t" + mutations + "\n")
-            list_stability.append(dG)
-        else:
-            nontrunk_ddG = float(trunk_line[1])
-            nontrunk_ddG_list.append(nontrunk_ddG)
-    trunk_average = np.average(trunk_ddG_list)
-    nontrunk_average = np.average(nontrunk_ddG_list)
-    print("trunk average ddG = " + str(trunk_average))
-    print("non-trunk average ddG = " + str(nontrunk_average))
-    return list_stability
+# plot the main trunk pathway with annotated mutations
+def plot_trunk_stability(trunk_location, tt_line, mutations):
+    plt.plot(trunk_location, tt_line, marker='o', color='blue')
+    plt.annotate(mutations, (trunk_location[1], tt_line[1]), color='pink', size='xx-small')
 
-def plot_stability(list_stability):
-    plt.plot(list_stability, marker='o')
-
-    plt.show()
+# plot the side branches off the main trunk pathway with annotated mutations
+def plot_branch_stability(trunk_location, tb_line, mutations):
+    plt.plot([trunk_location, trunk_location + 1], tb_line, color='red', marker='v')
+    plt.annotate(mutations, (trunk_location + 1, tb_line[1]), color='green', size='xx-small')
 
 # classify the transition between each local parent and child node
 def classify(parent_classify, child_classify):
@@ -62,19 +40,23 @@ def classify(parent_classify, child_classify):
             transition_classification = "TT"
         else:
             transition_classification = "TB"
-    else:
+    elif child_classify == "False":
         transition_classification = "BB"
+    else:
+        transition_classification = "Root"
     return transition_classification
 
+# add each ddG value to the corresponding list of ddG values
 def add_ddG_classify_list(classification, ddG):
     if classification == "TT":
         TT_ddG_list.append(ddG)
     elif classification == "TB":
         TB_ddG_list.append(ddG)
-    else:
+    elif classification == "BB":
         BB_ddG_list.append(ddG)
 
-def print_summary_ddG(file):
+# print the averages of the ddG lists for each classification
+def print_summary_ddG():
     TT_average = np.average(TT_ddG_list)
     TB_average = np.average(TB_ddG_list)
     BB_average = np.average(BB_ddG_list)
@@ -87,40 +69,65 @@ def print_summary_ddG(file):
     transition_ddG_file.write("Trunk -> Branch transitions = " + str(TB_average) + "\n")
     transition_ddG_file.write("Branch -> Branch transitions = " + str(BB_average) + "\n")
 
+# tracks the main trunk stability values, used to plot main trunk and sidebranches
+def update_stability_pathway(transition_ddG, stability_pathway):
+    most_recent_dG = float(stability_pathway[len(stability_pathway) - 1])
+    new_stability = most_recent_dG + transition_ddG
+    stability_pathway.append(new_stability)
 
-def main():
-    #make_run_file("_formatted_1")
-    #os.system("./foldx3b6 -runfile stability_runfile.txt")
-    #stability = get_stability()
-    #list_stability = get_trunk_ddG(stability)
-    #plot_stability(list_stability)
-    ddG_mutation = "ddG_mutations.txt"
-    ddG_mutation_file = open(ddG_mutation, 'r')
-    for line in ddG_mutation_file:
-        split_line = line.strip("\n").split("\t")
-
-        parent_classify = split_line[0]
-        child_classify = split_line[3]
-        transition_classification = classify(parent_classify, child_classify)
-
+# goes through each line of parent and child mutations and ddG to just give the corresponding transition ddGs and mutations
+# organizes for plotting
+def get_translation_info(line, stability_pathway, trunk_identifier):
+    split_line = line.strip("\n").split("\t")
+    parent_classification = split_line[0]
+    child_classification = split_line[3]
+    transition_classification = classify(parent_classification, child_classification)
+    if transition_classification != "Root":
         parent_ddG = float(split_line[1])
         child_ddG = float(split_line[4])
         transition_ddG = child_ddG - parent_ddG
         add_ddG_classify_list(transition_classification, transition_ddG)
 
         parent_mutations = split_line[2]
-        # delete these after the first run since fixed in tree_mutations
-        parent_mutations = parent_mutations[:len(parent_mutations) - 1]
         parent_mutations_set = set(parent_mutations.split(","))
         child_mutations = split_line[5]
-        # delete these after the first run since fixed in tree_mutations
-        child_mutations = child_mutations[:len(child_mutations) - 1]
         child_mutations_set = set(child_mutations.split(","))
-        transition_mutations = child_mutations_set - parent_mutations_set
+        transition_mutations_set = child_mutations_set - parent_mutations_set
 
-        transition_ddG_file.write(transition_classification + "\t" + str(transition_ddG) + "\t" + ','.join(transition_mutations) + "\n")
-    print_summary_ddG(ddG_mutation_file)
+        if transition_classification == "TT":
+            update_stability_pathway(transition_ddG, stability_pathway)
+            index = [len(stability_pathway) - 2, len(stability_pathway) - 1]
+            tt_line = [stability_pathway[index[0]], stability_pathway[index[1]]]
+            plot_trunk_stability(index, tt_line, ','.join(transition_mutations_set))
+            trunk_identifier.append(parent_mutations)
+
+        elif transition_classification == "TB":
+            # looks for the side branches that branch off a main trunk node
+            if parent_mutations in trunk_identifier:
+                trunk_location = trunk_identifier.index(parent_mutations)
+                tb_line = [stability_pathway[trunk_location], stability_pathway[trunk_location] + transition_ddG]
+                plot_branch_stability(trunk_location, tb_line, ','.join(transition_mutations_set))
+
+
+        transition_ddG_file.write(transition_classification + "\t" + str(transition_ddG) + "\t" + ','.join(transition_mutations_set) + "\t" + parent_mutations + "\n")
+
+def main():
+    make_run_file("_formatted_1")
+    #os.system("./foldx3b6 -runfile stability_runfile.txt")
+    stability = get_stability()
+    stability_pathway = [stability]
+
+    ddG_mutation = "ddG_mutations.txt"
+    ddG_mutation_file = open(ddG_mutation, 'r')
+    trunk_identifier = []
+    for line in ddG_mutation_file:
+        get_translation_info(line, stability_pathway, trunk_identifier)
+    print_summary_ddG()
     ddG_mutation_file.close()
+    transition_ddG_file.close()
+    plt.show()
+
+
 transition_ddG = "transition_ddG_mutations.txt"
 transition_ddG_file = open(transition_ddG, 'w')
 TT_ddG_list = []
