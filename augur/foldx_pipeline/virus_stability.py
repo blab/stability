f4988c8c6cfb10719b27033755feb3cf018cfc94
Charlton Callender <chacalle@uw.edu>
2015-12-08 17:40:52 -0800

    initialize outgroup sequence

--- augur/foldx_pipeline/virus_stability.py
+++ augur/foldx_pipeline/virus_stability.py
@@ -1,4 +1,6 @@
-
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import generic_dna
 
 
 
@@ -7,7 +9,7 @@
 class virus_stability:
 
 
-    def __init__(self, hash_code, accension, seq, trunk, tip):
+    def __init__(self, hash_code, accension, trunk, tip, seq):
 
         self.accession = accension
         self.hash_code = hash_code
@@ -18,18 +20,50 @@ class virus_stability:
         self.ddg_outgroup = None
         self.ddg_parent = None
 
-        #self.outgroup_seq =
+        self.structure_1HA0_seq = "STATLCLGHHAVPNGTLVKTITDDQIEVTNATELVQSSSTGKICNNPHRILDGIDCTLIDALLGDPHCDVFQNETWDLFVERSKAFSNCYPYDVPDYASLRSLVASSGTLEFITEGFTWTGVTQNGGSNACKRGPGSGFFSRLNWLTKSGSTYPVLNVTMPNNDNFDKLYIWGIHHPSTNQEQTSLYVQASGRVTVSTRRSQQTIIPNIGSRPWVRGLSSRISIYWTIVKPGDVLVINSNGNLIAPRGYFKMRTGKSSIMRSDAPIDTCISECITPNGSIPNDKPFQNVNKITYGACPKYVKQNTLKLATGMRNVPEKQTQGLFGAIAGFIENGWEGMIDGWYGFRHQNSEGTGQAADLKSTQAAIDQINGKLNRVIEKTNEKFHQIEKEFSEVEGRIQDLEKYVEDTKIDLWSYNAELLVALENQHTIDLTDSEMNKLFEKTRRQLRENAEEMGNGCFKIYHKCDNACIESIRNGTYDHDVYRNEALNNRFQI"
+        self.structure_2YP7_seq = "STATLCLGHHAVPNGTIVKTITNDQIEVTNATELVQSSSTGGICDSPHQILDGENCTLIDALLGDPQCDGFQNKKWDLFVERSKAYSNCYPYDVPDYASLRSLVASSGTLEFNNESFNWTGVTQNGTSSACKRKSNNSFFSRLNWLTHLKFKYPALNVTMPNNEKFDKLYIWGVHHPGTDNDQIFLYAQASGRITVSTKRSQQTVIPNIGSRPRVRNIPSRISIYWTIVKPGDILLINSTGNLIAPRGYFKIRSGKSSIMRSDAPIGKCNSECITPNGSIPNDKPFQNVNRITYGACPRYVKQNTLKLATGMRNVPEKQTQGIFGAIAGFIENGWEGMVDGWYGFRHQNSEGIGQAADLKSTQAAINQINGKLNRLIGKTNEKFHQIEKEFSEVEGRIQDLEKYVEDTKIDLWSYNAELLVALENQHTIDLTDSEMNKLFERTKKQLRENAEDMGNGCFKIYHKCDNACIGSIRNGTYDHDVYRDEALNNRFQI"
+
+        # get outgroup amino_acid sequence
+        try:
+            temp_outgroup = SeqIO.read('source-data/H3N2_outgroup.gb', 'genbank')
+        except:
+            print("could not find source-data/H3N2_outgroup.gb which contained the outgroup file")
+        dna_seq = str(temp_outgroup.seq).upper()
+        coding_dna = Seq(dna_seq, generic_dna)
+        protein = coding_dna.translate()
+        self.outgroup_seq = str(protein)
 
     def __str__(self):
 
         print("accession: " + self.accession)
-        print("hash: " + self.hash)
+        print("hash: " + self.hash_code)
         print("seq: " + self.seq)
         print("trunk: " + self.trunk)
         print("tip: " + self.tip)
 
     def align_to_outgroup(self):
         # align outgroup and self.seq, return mutations from outgroup -> seq
+        print("Aligning the protein structure sequence to the root sequence")
+        mutations_set = set()
+        for root_index in range(len(root_sequence)):
+            structure_index = root_index
+            site = root_index + 9  # for both 1HA0 and 2YP7
+            '''
+            if pdb_name.startswith("4WE4"):
+                site = root_index + 9
+            elif pdb_name.startswith("2HMG"):
+                site = root_index + 1
+            '''
+            '''
+                if root_index > 328:
+                    structure
+            '''
+            #print(root_sequence[index] + " : " + structure_sequence[index])
+            if root_sequence[root_index] != structure_sequence[structure_index]:
+                mutation = structure_sequence[structure_index] + str(site) + root_sequence[root_index]
+                list_mutations.append(mutation)
+        report_mutations = ','.join(list_mutations)
+        return report_mutations
 
     def calculate_ddg_outgroup(self):
         # read the ddG from the output file of most recent foldx run
